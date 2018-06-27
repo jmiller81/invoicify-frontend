90d9bdadf151122c6d9c1571b619cf62aa6c9ff9
MZS02B@ALLYIPOC.COM <MZS02B@ALLYIPOC.COM>
2018-06-27 15:26:01 -0400

    routing module changes

--- src/app/routing/routing.module.ts
+++ src/app/routing/routing.module.ts
@@ -4,6 +4,8 @@ import { RouterModule, Routes } from '@angular/router';
 import { CompanyComponent }   from '../company/company.component';
 import { CompanyFormComponent }   from '../company-form/company-form.component';
 import { HomeComponent }   from '../home/home.component';
+import { UserComponent } from '../user/user.component';
+import { BillingRecordComponent } from '../billing-record/billing-record.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
@@ -11,6 +13,8 @@ const routes: Routes = [
   { path: 'company',  component: CompanyComponent },
   { path: 'company/edit/:id', component: CompanyFormComponent },
   { path: 'company/add', component: CompanyFormComponent },
+  { path: 'user', component: UserComponent},
+  { path: 'billing-record', component: BillingRecordComponent}
 ];
 
 @NgModule({
